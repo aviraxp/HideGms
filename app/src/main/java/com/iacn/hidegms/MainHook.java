6bd1651c324c186380bd2e7f566311892ba4920e
Wang Han <416810799@qq.com>
2016-10-14 06:50:20 -0500

    Handle Errors

--- app/src/main/java/com/iacn/hidegms/MainHook.java
+++ app/src/main/java/com/iacn/hidegms/MainHook.java
@@ -7,28 +7,33 @@ import android.content.DialogInterface.OnCancelListener;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 /**
  * Created by iAcn on 2016/8/4
  * Emali iAcn0301@foxmail.com
  */
+
 public class MainHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-
-        findAndHookMethod("com.google.android.gms.common.GooglePlayServicesUtil", loadPackageParam.classLoader,
-                "getErrorDialog", int.class, Activity.class, int.class, OnCancelListener.class, new XC_MethodReplacement() {
-                    @Override
-                    protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                        return new Dialog((Context) methodHookParam.args[1]) {
-                            @Override
-                            public void show() {
-                            }
-                        };
-                    }
-                });
+       try {
+           XposedHelpers.findAndHookMethod("com.google.android.gms.common.GooglePlayServicesUtil", loadPackageParam.classLoader,
+                   "getErrorDialog", int.class, Activity.class, int.class, OnCancelListener.class, new XC_MethodReplacement() {
+                       @Override
+                       protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                           return new Dialog((Context) methodHookParam.args[1]) {
+                               @Override
+                               public void show() {
+                               }
+                           };
+                       }
+                   });
+           XposedBridge.log("Hide GMS Success");
+       } catch (Throwable t) {
+           return;
+       }
     }
 }
